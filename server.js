8991f58ab3fada0554f67db0292dc69f5273f667
Santos-Kleber <kleber.b.s@gmail.com>
2021-11-15 12:50:42 -0300

    Ajustando server com cors

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 //Importa as dependências que acabamos de instalar
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
 
 const app = express();
 
 // Acrescentando cabecalho para resolver problema de CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://app-angular-product.herokuapp.com/");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
+//app.use(function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "https://app-angular-product.herokuapp.com/");
+//  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//  next();
+//});
+
+app.use(cors({Origin: 'https://app-angular-product.herokuapp.com/'}));
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + "/dist/angular-http"));
